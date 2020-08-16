49696eb718cf2f40fb85808feb126cda02296611
Cfomodz <ashby45@byui.edu>
2020-08-16 15:19:52 -0600

    Fixed bugs, added functionality
    Uncommented customization, fixed title, added print statements

--- videoMaker.py
+++ videoMaker.py
@@ -2,13 +2,17 @@
 from moviepy.config import change_settings
 change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.0.10-Q16-HDRI\magick.exe"})
 
-from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips, TextClip, CompositeVideoClip, concatenate_audioclips
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips, TextClip, CompositeVideoClip, concatenate_audioclips, AudioClip, CompositeAudioClip
 import os
 
-fr = 24
-#fr = input("Enter Desired Framerate: ")
-output_name = "final_video.mp4"
-#output_name = input("Enter Desired Output Name:")
+fr = input("Enter Desired Framerate, leave blank and hit enter for default(24): ")
+if fr == "":
+    fr = 24
+else: fr = int(fr)
+    
+output_name = input("Enter Desired Output Name, leave blank and hit enter for default(final_video.mp4): ")
+if output_name == "":
+    output_name = "final_video.mp4"
 
 video_files = []
 
@@ -21,8 +25,11 @@ for i in os.listdir():
 for i, clip in enumerate(video_files):
     video_files[i] = VideoFileClip(clip)
 
-print(video_files)
-    
+print("Number of video files loaded: " + str(len(video_files)))
+
+#title frontend
+title = input("Enter Title Screen Text, leave blank and hit enter to skip: ")
+
 #endscreen
 endtext = input("Enter Endscreen Text, leave blank and hit enter to skip: ")
 if endtext != "":
@@ -31,13 +38,14 @@ if endtext != "":
 #combine video clips
 combined_clip = concatenate_videoclips(video_files, method="compose")
 
-#title
-title = input("Enter Title Screen Text, leave blank and hit enter to skip: ")
+#title backend
 if title != "":
     txt_clip = TextClip(title,fontsize=70,color='white')
-    txt_clip = txt_clip.set_pos('center').set_duration(10)
-    combined_clip = CompositeVideoClip([combined_clip, txt_clip])
-
+    txt_clip = txt_clip.set_pos('bottom').set_duration(5)
+    video = CompositeVideoClip([combined_clip, txt_clip])
+else:
+    video = combined_clip
+    
 #audio
 audio_files = []
 
@@ -45,7 +53,7 @@ for i in os.listdir():
     if i.endswith(".mp3") or i.endswith(".wav"):
         audio_files.append(i)
 
-print(audio_files)
+print("Audio files loaded are: " + str(audio_files))
 
 for i, clip in enumerate(audio_files):
     audio_files[i] = AudioFileClip(clip)
@@ -54,8 +62,14 @@ for i, clip in enumerate(audio_files):
 combined_audio = concatenate_audioclips(audio_files)
 
 #Set Duration of audioclip
-audioclip = combined_audio.set_duration(combined_clip.duration)
-final_clip = combined_clip.set_audio(audioclip)
+background_audio = combined_audio.set_duration(video.duration)
+
+#combine videos' audio and audio track
+video_audio = video.audio
+print(background_audio)
+print(video_audio)
+final_audio = CompositeAudioClip([background_audio, video_audio])
+final_clip = video.set_audio(final_audio)
 
 #render
 print("Composition successful. Rendering!")
